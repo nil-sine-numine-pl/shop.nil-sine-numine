38b60b28c68fe5f80f4babf4f5661e44a78f8a3a
Marcin <marcingolenia@gmail.com>
2021-02-24 13:18:39 +0100

    Init foundation shop gatsby site

---
+++ src/pages/404.jsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+export default function NotFoundPage() {
+  return (
+    <main>
+      <title>Not found</title>
+      <h1>Page not found</h1>
+      <p>
+        Sorry{" "}
+        <span role="img" aria-label="Pensive emoji">
+          😔
+        </span>{" "}
+        we couldn’t find what you were looking for.
+        <br />
+        <br />
+        <Link to="/">Go home</Link>.
+      </p>
+    </main>
+  )
+}
