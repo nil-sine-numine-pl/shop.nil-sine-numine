d9969a3163369086ae85dacbe977032ab218a4f7
Marcin 👾 Golenia <mgolenia@cybervadis.com>
2021-04-14 18:59:07 +0200

    add logo to page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
@@ -10,6 +11,7 @@ const ListLink = props => (
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <img src="../../img/logo.png" alt="logo"></img>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>MySweetSite</h3>
