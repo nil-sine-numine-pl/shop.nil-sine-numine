96dd280c55be8669d9e5eaaa6d606d9be7701631
Marcin 👾 Golenia <mgolenia@cybervadis.com>
2021-04-17 00:29:13 +0200

    More styling, example image

--- src/components/colors.ts
+++ src/components/colors.ts
@@ -1,7 +1,8 @@
 const Colors = {
     background: '#ededed',
     card: 'white',
-    primary: '#ededed',
+    primary: 'rgb(110,61,50)',
+    font: 'black'
   }
 
 export default Colors
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,35 +5,46 @@ import { Global } from '@emotion/core'
 import Colors from './colors'
 import styled from '@emotion/styled'
 
-const Links = styled.ul({
-  listStyle: `none`,
+const Links = styled.nav({
+  margin: `1rem 1rem 3rem 1rem`,
   a: {
-    textDecoration: `none`
-  }
+    textDecoration: `none`,
+    margin: `0rem 1.5rem`,
+    fontSize: `0.9rem`,
+  },
+  '& :visited': { color: Colors.font},
+  '.active-link': {color: Colors.primary, textDecoration: `underline`}
 })
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+  const Footer = styled.footer({
+    color: `white`,
+    backgroundColor: Colors.primary,
+    textAlign: `center`,
+    marginTop: `4rem`,
+    padding: `2rem`,
+  })
 
 export default function Layout({ children }) {
   return (
+    <div>
     <div style={{ margin: `1rem auto`, maxWidth: 800}}>
-      <Global styles={{body: {backgroundColor: Colors.background, fontFamily: '"Open Sans", Helvetica, Arial'}}}/>
+      <Global styles={{body: {margin: 0, backgroundColor: Colors.background, color: Colors.font, fontFamily: '"Open Sans", Helvetica, Arial', overflowY: `scroll`}}}/>
       <header style={{ marginBottom: `1.5rem`, textAlign: `center` }}>    
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <StaticImage src="../images/logo.png" alt="" placeholder="blurred"  />
         </Link>  
         <Links>
-          <ListLink to="/">FUNDACJA</ListLink>
-          <ListLink to="/shop/">SKLEP</ListLink>
-          <ListLink to="/blog/">BLOG</ListLink>
-          <ListLink to="/about/">O NAS</ListLink>
+          <Link activeClassName='active-link' to="/">KIM JESTEŚMY?</Link>
+          <Link activeClassName='active-link' to="/shop/">SKLEP</Link>
+          <Link activeClassName='active-link' to="/blog/">BLOG I NEWSY</Link>
+          <Link activeClassName='active-link' to="/about/">O NAS</Link >
         </Links>
       </header>
       {children}
+      </div>
+      <Footer>
+        Prawa autorskie © 2021  — Nil sine numine
+      </Footer>
     </div>
   )
 }
