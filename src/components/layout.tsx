e51d68427b9605b3a9970c558f53ee882634af0f
Marcin 👾 Golenia <mgolenia@cybervadis.com>
2021-04-17 19:52:51 +0200

    Minor work with layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,38 +1,91 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image'
 import { Global } from '@emotion/core'
 import Colors from './colors'
 import styled from '@emotion/styled'
+import facebook from '../images/facebook.svg'
 
 const Links = styled.nav({
   margin: `1rem 1rem 3rem 1rem`,
   a: {
     textDecoration: `none`,
-    margin: `0rem 1.5rem`,
+    margin: `1rem 1.5rem`,
     fontSize: `0.9rem`,
+    '@media (orientation: portrait)': {display: `block`},
   },
   '& :visited': { color: Colors.font},
-  '.active-link': {color: Colors.primary, textDecoration: `underline`}
+  '.active-link': {color: Colors.primary, textDecoration: `underline`},
+})
+const Icon = styled.img({
+  width: '2rem',
+  height: '2rem', 
+  color: 'white'
+})
+const Footer = styled.footer({
+  color: `white`,
+  backgroundColor: Colors.primary,
+  textAlign: `center`,
+  marginTop: `4rem`,
+  padding: `2rem`,
+})
+const Navbar = styled.div({})
+const Menu = styled.div({
+  '@media (orientation: landscape)': {display: `none`},
+  marginTop: '.5rem',
+  float: 'right',
+  width:'2rem', 
+  height: '2rem',
+  cursor: 'pointer',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: 'all .5s ease-in-out',
+  //border: `3px solid ${Colors.font}`
+})
+const Hamburger = styled.div({
+  width: '1.5rem',
+  height: `.1rem`,
+  position:'relative',
+  backgroundColor: Colors.font,
+  '&::after, &::before': {
+    content: '""',
+    position: 'absolute',
+    left: '0',
+    width: '1.5rem',
+    height: `.1rem`,
+    backgroundColor: Colors.font,
+  },
+  '&::after': {transform: 'translateY(-0.5rem)'},
+  '&::before': {transform: 'translateY(0.5rem)'},
+  '.open': {
+    transform: 'translateX(-2rem)',
+    background:'transaprent',
+  }
 })
 
-  const Footer = styled.footer({
-    color: `white`,
-    backgroundColor: Colors.primary,
-    textAlign: `center`,
-    marginTop: `4rem`,
-    padding: `2rem`,
-  })
 
 export default function Layout({ children }) {
+  let navBarActiveClass = ''
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    navBarActiveClass = active ? 'is-active' : ''
+    console.log(navBarActiveClass)
+  })
+
   return (
     <div>
-    <div style={{ margin: `1rem auto`, maxWidth: 800}}>
+    <div style={{ padding: `1rem`, margin:`auto`, maxWidth: 800}}>
       <Global styles={{body: {margin: 0, backgroundColor: Colors.background, color: Colors.font, fontFamily: '"Open Sans", Helvetica, Arial', overflowY: `scroll`}}}/>
-      <header style={{ marginBottom: `1.5rem`, textAlign: `center` }}>    
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <StaticImage src="../images/logo.png" alt="" placeholder="blurred"  />
-        </Link>  
+      <header style={{ textAlign: `center` }}>    
+        <Navbar>
+          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            <StaticImage src="../images/logo.png" alt="" placeholder="blurred"  />
+          </Link>
+          <Menu onClick={() => setActive(!active)}>
+            <Hamburger/>
+          </Menu>
+        </Navbar>
         <Links>
           <Link activeClassName='active-link' to="/">KIM JESTEŚMY?</Link>
           <Link activeClassName='active-link' to="/shop/">SKLEP</Link>
@@ -43,6 +96,10 @@ export default function Layout({ children }) {
       {children}
       </div>
       <Footer>
+      <a title="facebook" href="https://www.facebook.com/KurnikPolski/" target="_blank" rel="noopener noreferrer">
+        <Icon src={facebook} alt="Facebook"/>
+      </a>
+      <br/>
         Prawa autorskie © 2021  — Nil sine numine
       </Footer>
     </div>
