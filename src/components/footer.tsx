eeba30a564e36c886ebbbc98724f425a21f024c8
Marcin <marcingolenia@gmail.com>
2021-12-11 17:28:28 +0100

    Change BA Number

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -26,46 +26,47 @@ const FooterGrid = styled.div({
 
 export const Footer = () => {
   const { footerFileUrl } = useSiteMetadata()
-  return(
+  return (
     <StyledFooter>
-      <section style={{maxWidth: Sizes.pageMaxWidth, margin: 'auto auto auto auto'}}>
+      <section style={{ maxWidth: Sizes.pageMaxWidth, margin: 'auto auto auto auto' }}>
         <Lined >
-          <StaticImage style={{margin: '0 1rem 1.5rem 1rem'}} src="../images/footer_logo.webp" alt="mail" placeholder="blurred"/>
+          <StaticImage style={{ margin: '0 1rem 1.5rem 1rem' }} src="../images/footer_logo.webp" alt="mail" placeholder="blurred" />
         </Lined>
         <FooterGrid>
-          <table style={{ borderSpacing: '0'}}>
-          <tbody>
-          <tr>
-            <td style={{width:'50%'}}><Link aria-label="O Fundacji" to="/about/">O FUNDACJI</Link ></td>
-            <td><Link aria-label="Sklep" to="/shop/">SKLEP</Link></td>
-          </tr>
-          <tr>
-            <td><Link aria-label="Blog" to="/blog/">BLOG</Link></td>
-            <td><Link aria-label="Kontakt" to="/contact/">KONTAKT</Link ></td>
-          </tr>
-          <tr>
-            <td><Link aria-label="Pomagaj z nami" to="/">POMAGAJ Z NAMI</Link></td>
-            <td><a title="polityka prywatności" target="_blank" href={footerFileUrl} rel="noopener noreferrer">POLITYKA PRYWATNOŚCI</a></td>
-          </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-          <tr>
-            <td style={{width:'50%'}}>FUNDACJA NIL SINE NUMINE</td>
-            <td>Numer konta bankowego:</td>
-          </tr>
-          <tr>
-            <td style={{fontFamily:'montserrat', fontSize: '0.85rem'}}>Nowe Miasto 10, 16-080 Tykocin</td>
-            <td style={{fontFamily:'montserrat', fontSize: '0.85rem'}}>07 8060 0004 0710 1531 2000 0010</td>
-          </tr>
-          <tr>
-            <td><img src={kontaktSvg}></img></td>
-            <td><a title="polityka prywatności" target="_blank" href="/statut.pdf" rel="noopener noreferrer">Statut fundacji</a></td>
-          </tr>
-        </tbody>
-        </table>
-      </FooterGrid>
+          <table style={{ borderSpacing: '0' }}>
+            <tbody>
+              <tr>
+                <td style={{ width: '50%' }}><Link aria-label="O Fundacji" to="/about/">O FUNDACJI</Link ></td>
+                <td><Link aria-label="Sklep" to="/shop/">SKLEP</Link></td>
+              </tr>
+              <tr>
+                <td><Link aria-label="Blog" to="/blog/">BLOG</Link></td>
+                <td><Link aria-label="Kontakt" to="/contact/">KONTAKT</Link ></td>
+              </tr>
+              <tr>
+                <td><Link aria-label="Pomagaj z nami" to="/">POMAGAJ Z NAMI</Link></td>
+                <td><a title="polityka prywatności" target="_blank" href={footerFileUrl} rel="noopener noreferrer">POLITYKA PRYWATNOŚCI</a></td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+              <tr>
+                <td style={{ width: '50%' }}>FUNDACJA NIL SINE NUMINE</td>
+                <td>Numer konta bankowego:</td>
+              </tr>
+              <tr>
+                <td style={{ fontFamily: 'montserrat', fontSize: '0.85rem' }}>Nowe Miasto 10, 16-080 Tykocin</td>
+                <td style={{ fontFamily: 'montserrat', fontSize: '0.85rem' }}>61 1140 2004 0000 3002 8142 5365</td>
+              </tr>
+              <tr>
+                <td><img src={kontaktSvg}></img></td>
+                <td><a title="polityka prywatności" target="_blank" href="/statut.pdf" rel="noopener noreferrer">Statut fundacji</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </FooterGrid>
       </section>
     </StyledFooter>
-  )}
+  )
+}
